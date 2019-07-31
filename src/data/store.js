82ddd2f476f471ab8742fa8b1a47dfcdc2f8e664
ollie <leyechi@gmail.com>
2019-07-31 12:53:45 -0700

    added store

---
+++ src/data/store.js
@@ -0,0 +1,19 @@
+import products from './products.js';
+
+const store = {
+    storage: window.localStorage,
+
+    save(key, item) {
+        const json = JSON.stringify(item);
+        store.storage.setItem(key, json);
+    },
+
+    get(key) {
+        const json = store.storage.getItem(key);
+        const item = JSON.parse(json);
+
+        return item;
+    }
+}
+
+export default store;
