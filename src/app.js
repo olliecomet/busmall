fa57a495a81c00a60c29569c3754056275a171d6
ollie <leyechi@gmail.com>
2019-08-01 16:38:34 -0700

    tracking rounds

--- src/app.js
+++ src/app.js
@@ -1,18 +1,41 @@
-import products from './data/products.js';
+import store from './data/store.js';
 import Listing from './data/listings.js'; 
-
 import renderInput from './render-products.js';
 
 const left = document.getElementById('left');
 const middle = document.getElementById('middle');
 const right = document.getElementById('right');
+const radioInputs = document.querySelectorAll('input[name=product]');
+const counter = document.getElementById('round-counter');
 
+let rounds = 0;
+let live = true;
+let selectedProducts = [];
+
+const products = store.getProducts();
 const mainListing = new Listing(products);
-
 const firstThreeProducts = mainListing.getThreeRandomProducts();
 
 const leftInput = renderInput(left, firstThreeProducts[0]);
 const middleInput = renderInput(middle, firstThreeProducts[1]);
 const rightInput = renderInput(right, firstThreeProducts[2]);
 
-// add for loop to add event listeners to all input things
+for(let i = 0; i < radioInputs.length; i++){
+    const radioInput = radioInputs[i];
+    radioInput.addEventListener('input', handleUserChoice);
+}
+
+function handleUserChoice(event) {
+    if(!live) {
+        return;
+    }
+
+    
+
+    rounds++;
+    counter.textContent = rounds;
+
+    if(rounds === 25) {
+        live = false;
+    }
+}
